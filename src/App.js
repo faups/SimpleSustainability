b6fc29fc1e427971431ea76852f761d98c4e3698
Luke Faupel <lukefaupel@gmail.com>
2020-11-28 18:11:50 -0600

    updated form references

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useForm } from "react-hook-form";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
-function App() {
+export default function App() {
+  const { register, handleSubmit } = useForm();
+
+  const config = {
+    apiKey: "AIzaSyD__P-Qqh4J3U337Nr_G5oQjeGCsC2H93M",
+    authDomain: "simple-sustainability.firebaseapp.com",
+    databaseURL: "https://simple-sustainability.firebaseio.com",
+    projectId: "simple-sustainability",
+    storageBucket: "simple-sustainability.appspot.com",
+    messagingSenderId: "382171174450",
+    appId: "1:382171174450:web:324f2134da1be6bc9e0e2b",
+    measurementId: "G-LTW5YQ2G3E"
+  };
+  if (!firebase.apps.length) {
+    firebase.initializeApp(config);
+  }
+  var db = firebase.firestore();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    db.collection("forms").add({
+      waterUsage: data.waterUsage,
+      energyUsage: data.energyUsage,
+      email: data.email
+    });
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <label htmlFor="waterUsage">Water Usage (Gallons)</label>
+          <input name="waterUsage" placeholder="49" ref={register} />
+        </div>
+
+        <div>
+          <label htmlFor="energyUsage">Energy Usage (kWh)</label>
+          <input name="energyUsage" placeholder="909" ref={register} />
+        </div>
+
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            name="email"
+            placeholder="simplesustainability@gmail.com"
+            type="email"
+            ref={register}
+          />
+        </div>
+        <input type="submit" />
+      </form>
     </div>
   );
 }
-
-export default App;
