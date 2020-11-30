b138e490221551191762d1b7bb408ef016c50a6f
Luke Faupel <lukefaupel@gmail.com>
2020-11-29 19:54:01 -0600

    updated signin

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,36 +1,19 @@
 import "./index.css";
 import React from "react";
 import { useForm } from "react-hook-form";
-import firebase from "firebase/app";
-import "firebase/firestore";
 
 export default function App() {
   const { register, handleSubmit } = useForm();
 
   document.title = "Sustainability Simplified";
 
-  const config = {
-    apiKey: process.env.REACT_APP_API_KEY,
-    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_DATABASE_URL,
-    projectId: process.env.REACT_APP_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_APP_ID,
-    measurementId: process.env.REACT_APP_MEASUREMENT_ID
-  };
-  if (!firebase.apps.length) {
-    firebase.initializeApp(config);
-  }
-  var db = firebase.firestore();
-
   const onSubmit = (data) => {
     console.log(data);
-    db.collection("forms").add({
-      waterUsage: data.waterUsage,
-      energyUsage: data.energyUsage,
-      email: data.email
-    });
+    // db.collection("forms").add({
+    //   waterUsage: data.waterUsage,
+    //   energyUsage: data.energyUsage,
+    //   email: data.email
+    // });
   }
 
   return (
