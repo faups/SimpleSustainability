8a56cc51c89177fe14e74f2bedb078d4322ca5db
Luke Faupel <lukefaupel@gmail.com>
2020-12-10 13:35:59 -0600

    updated header

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
+import tsclogo from '../../assets/tsclogo.jpg';
+import './index.css';
+
 import SignOutButton from '../SignOut';
 import * as ROUTES from '../../constants/routes';
 import { AuthUserContext } from '../Session';
@@ -16,25 +19,55 @@ const Navigation = () => (
 );
 
 const NavigationAuth = () => (
-  <div>
-    <Link to={ROUTES.LANDING}>Landing</Link>
-    <code> </code>
-    <Link to={ROUTES.HOME}>Home</Link>
-    <code> </code>
-    <Link to={ROUTES.ACCOUNT}>Account</Link>
-    <code> </code>
-    <Link to={ROUTES.ADMIN}>Admin</Link>
-    <code> </code>
-    <SignOutButton />
-  </div>
+  // <div>
+  //   <Link to={ROUTES.LANDING}>Landing</Link>
+  //   <code> </code>
+  //   <Link to={ROUTES.HOME}>Home</Link>
+  //   <code> </code>
+  //   <Link to={ROUTES.ACCOUNT}>Account</Link>
+  //   <code> </code>
+  //   <Link to={ROUTES.ADMIN}>Admin</Link>
+  //   <code> </code>
+  //   <SignOutButton />
+  // </div>
+  <header>
+    <img src={tsclogo} alt="tsclogo" />
+    <nav>
+      <ul>
+        <li>
+          <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.HOME}>Home</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.ACCOUNT}>Account</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.ADMIN}>Admin</Link>
+        </li>
+        <li>
+          <SignOutButton />
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 const NavigationNonAuth = () => (
-  <div>
-    <Link to={ROUTES.LANDING}>Landing</Link>
-    <code> </code>
-    <Link to={ROUTES.SIGN_IN}>Sign In</Link>
-  </div>
+  <header>
+    <img src={tsclogo} alt="tsclogo" />
+    <nav>
+      <ul>
+        <li>
+          <Link to={ROUTES.LANDING}>Landing</Link>
+        </li>
+        <li>
+          <Link to={ROUTES.SIGN_IN}>Sign In</Link>
+        </li>
+      </ul>
+    </nav>
+  </header>
 );
 
 export default Navigation;
